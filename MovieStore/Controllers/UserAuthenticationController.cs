c9390300e4eb79f6c89b26edec6d00619f8e9397
okancc <118931286+okancc@users.noreply.github.com>
2023-03-24 14:13:33 +0300

    Login ve Logout eklendi.

--- MovieStore/Controllers/UserAuthenticationController.cs
+++ MovieStore/Controllers/UserAuthenticationController.cs
@@ -12,20 +12,56 @@ namespace MovieStore.Controllers
             this.authService= authService;
 
         }
-        public async  Task<IActionResult> Register()
-        {
-            var model = new RegistrationModel
-            {
-                Email = "admin@gmail.com",
-                UserName = "admin",
-                Name = "Okan",
-                Password = "Admin@123",
-                PasswordConfirm = "Admin@123",
-                Role = "Admin",
+        //public async  Task<IActionResult> Register()
+        //{
+        //    var model = new RegistrationModel
+        //    {
+        //        Email = "admin@gmail.com",
+        //        UserName = "admin",
+        //        Name = "Okan",
+        //        Password = "Admin@123",
+        //        PasswordConfirm = "Admin@123",
+        //        Role = "Admin",
 
-            };
-           var result = await authService.RegisterAsync(model);
-            return Ok(result.Message);
+        //    };
+        //   var result = await authService.RegisterAsync(model);
+        //    return Ok(result.Message);
+        //}
+
+        public async Task<IActionResult> Login()
+        {
+            return View();
+             
         }
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View (model);
+            }
+            var result = await authService.LoginAsync(model);
+            if (result.StatusCode == 1)
+            {
+                return RedirectToAction("Index","Home");
+
+
+            }
+            else
+            {
+                TempData["msg"] = "Error on server side";
+                return RedirectToAction(nameof(Login));
+            }
+            
+        }
+
+        public async Task<IActionResult> Logout()
+        {
+          await authService.LogoutAsync();
+
+            return RedirectToAction(nameof(Login));
+
+        }
+
     }
 }
